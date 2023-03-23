e5ee85d718376dcefb1637901ba23d298828d813
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-03-23 15:56:11 +0100

    stylesheet does not work

--- lev3_1/index.js
+++ lev3_1/index.js
@@ -5,20 +5,27 @@ const sendFile = (path, res) =>{
     fs.readFile (path, (err, data) =>{
         
         if (err){
+            console.log(err)
             res.writeHead(500)
             res.end()
             return
         }
 
         if (path.includes('.jpeg')){
-            res.writeHead(200, {contentType: 'image/jpeg'})
+            console.log(data)
+            res.writeHead(200, {'Content-Type': 'image/jpeg'})
             res.end(data)
             return
+        } else if (path.includes('.ttf')){
+            res.writeHead(200, {'Content-Type': 'font/ttf'})
+            res.end(data)
+            return
+        }else{
+            res.writeHead(200, {'Content-Type': 'text/html', 'charset': 'utf8'})
+            res.write(data)
+            res.end()
         }
 
-        res.writeHead(200, {contentType: 'text/html', charset: 'utf8'})
-        res.write(data)
-        res.end()
     })
 }
 
@@ -29,11 +36,18 @@ const reqHandler =(req,res) =>{
     if(req.url === '/'){
         sendFile('./assets/html/index.html',res)
         return
-    } else if (req.url === '/assets/css/style.css'){
-        sendFile('./assets/css/style.css',res)
+    } else if (req.url === '/style.css'){
+        sendFile('./assets/html'+ req.url,res)
     } 
+    else if(req.url.includes('.html')){
+        const filePath = './assets/html' + req.url
+        sendFile(filePath, res)
+    }
+    else if(req.url.includes('.ico')){
+        res.end()
+    }
     else{
-        const filePath = './assets/html/' + req.url
+        const filePath = './assets' + req.url
         sendFile(filePath, res)
     }
     
